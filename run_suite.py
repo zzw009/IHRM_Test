fde3ffdc09684306a0afede2dba8b14a6044f303
zzw <1468610035@qq.com>
2019-10-17 15:51:04 +0800

    v1.0

--- run_suite.py
+++ run_suite.py
@@ -1,7 +1,10 @@
 import unittest
 
+import time
+
 from case.TestIHRMEmploye import TestEmploye
 from case.TestIHRMUser import TestUser
+from tools.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.TestSuite()
 suite.addTest(TestUser("test_login_success"))
@@ -10,6 +13,9 @@ suite.addTest(TestEmploye("test_update_emp"))
 suite.addTest(TestEmploye("test_get_emp"))
 suite.addTest(TestEmploye("test_delete_emp"))
 
-runner = unittest.TextTestRunner()
-runner.run(suite)
-
+# 打开文件流
+file_to = "./report/report.html" + time.strftime("%Y-%m-%d %H-%M-%S") + ".html"
+with open(file_to, "wb") as f:
+    # 使用 HTMLTestRunner 要运行测试套件，将结果写入文件流
+    runner = HTMLTestRunner(f, title="测试报告", description="v1.0")
+    runner.run(suite)
